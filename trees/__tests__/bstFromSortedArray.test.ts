3547b36d56c91601220cc79bf5ef7553a68d0fda
alejo4373 <alejo4373@gmail.com>
2018-08-23 01:02:01 -0400

    add @types/jest + test file clean up

--- trees/__tests__/bstFromSortedArray.test.ts
+++ trees/__tests__/bstFromSortedArray.test.ts
@@ -5,7 +5,6 @@ describe('bstFromSortedArray', () => {
   it('Returns a binary search tree out of an array', () => {
     const values: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     let rootNode = btsBuilder(values);
-    rootNode.traverseInOrder();
     expect(rootNode.val).toBe(5);
     expect(rootNode.left.val).toBe(2);
     expect(rootNode.right.val).toBe(8);
