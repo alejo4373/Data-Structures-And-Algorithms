44bcc9e2c795a9ad47370817c6ffd94404d4fd2b
alejandro <alejo4373@gmail.com>
2019-04-20 09:26:24 -0400

    all diplicates test

--- linked-lists/problems/removeDuplicates/removeDuplicates.test.ts
+++ linked-lists/problems/removeDuplicates/removeDuplicates.test.ts
@@ -7,3 +7,10 @@ it('Removes duplicates out of linked list', () => {
   let cleanLl: LinkedList = removeDuplicates(ll);
   expect(cleanLl.printHumanReadable()).toBe('1->3->4->5->2->6->7->8->null')
 })
+
+it('Removes duplicates out of linked list', () => {
+  let array = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  let ll: LinkedList = new LinkedList().fromArray(array);
+  let cleanLl: LinkedList = removeDuplicates(ll);
+  expect(cleanLl.printHumanReadable()).toBe('1->null')
+})
